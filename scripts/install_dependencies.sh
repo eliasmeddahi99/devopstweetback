ede2a6f7cadd82b482ca9dd7c09f0b74b694ed3d
eliasmeddahi99 <168426559+eliasmeddahi99@users.noreply.github.com>
2024-11-26 02:49:21 +0100

    Update install_dependencies.sh

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,1 +1,9 @@
+#!/bin/bash
+# Aller dans le répertoire du backend
+cd /home/admin/backend
 
+# Construire l'image Docker
+docker build -t backend-app .
+
+# Lancer le conteneur Docker
+docker run -d -p 3000:3000 --name backend-app backend-app
